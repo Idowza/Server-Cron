d4cc629ccf39a6f451092c610b1ae84b387a559f
iDowza <steveniden@gmail.com>
2025-02-02 12:01:49 -0600

    Create cronupdate.sh

---
+++ cronupdate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+LOG_FILE="/home/steven/bin/log/update_$(date +%F+%T).txt"
+
+# Redirect stdout and stderr to the log file, and append if the file exists
+exec &>> "${LOG_FILE}"
+
+apt-get update -y
+apt-get upgrade -y
+flatpak update -y
+apt-get autoremove -y
+apt-get autoclean -y
+apt-get autopurge -y
+apt-get install -f -y
+dpkg --configure -a
+
+pihole -up
+pihole -g
